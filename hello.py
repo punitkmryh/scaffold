2e00cc7909f37b70540a937cb24b0802686d95fa
Punit <punitkmryh@gmail.com>
2024-05-07 07:02:43 +0000

    inital structure

---
+++ hello.py
@@ -0,0 +1,6 @@
+def add(x, y):
+    return x + y
+
+
+results = add(1, 2)
+print(f"This is the sum of 1,2 : {results}")
---
+++ test_hello.py
@@ -0,0 +1,5 @@
+from hello import add
+
+
+def test_add1():
+    assert add(1, 2) == 3
